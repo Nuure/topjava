54746446ef64e02a057baaa2e60a2913808dada7
alina.pirogova <alina.pirogova@team.wrike.com>
2023-06-13 21:12:07 +0300

    Implementation of MealService and MealRestController, fix auth user

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
@@ -17,16 +18,29 @@ import static java.util.stream.Collectors.toList;
 import static ru.javawebinar.topjava.util.DateTimeUtil.isBetweenHalfOpen;
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(this::saveTest);
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
+        if (meal.isNew()) {
+            meal.setId(counter.incrementAndGet());
+            repository.put(meal.getId(), meal);
+            return meal;
+        }
+        // handle case: update, but not present in storage
+        else if (meal.getUserId() == userId) {
+            return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        } else return null;
+    }
+
+    public Meal saveTest(Meal meal) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
             repository.put(meal.getId(), meal);
@@ -39,9 +53,9 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public boolean delete(int id) {
+    public boolean delete(int id, int userId) {
         try {
-            if (authUserId() == repository.get(id).getUserId()) {
+            if (userId == repository.get(id).getUserId()) {
                 repository.remove(id);
                 return true;
             }
@@ -53,9 +67,9 @@ public class InMemoryMealRepository implements MealRepository {
 
 
     @Override
-    public Meal get(int id) {
+    public Meal get(int id, int userId) {
         try {
-            if (authUserId() == repository.get(id).getUserId()) {
+            if (userId == repository.get(id).getUserId()) {
                 return repository.get(id);
             }
         } catch (NullPointerException e) {
@@ -65,13 +79,15 @@ public class InMemoryMealRepository implements MealRepository {
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return getFiltered(MIN, MAX);
+    public Collection<Meal> getAll(int userId) {
+        return getFiltered(userId, MIN, MAX);
     }
 
-    public Collection<Meal> getFiltered(LocalDateTime startDateTime, LocalDateTime endDateTime) {
+    @Override
+    public Collection<Meal> getFiltered(int userId, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         return repository.values()
                 .stream()
+                .filter(meal -> meal.getUserId() == userId)
                 .filter(meal -> isBetweenHalfOpen(meal.getDateTime(), startDateTime, endDateTime))
                 .sorted(comparing(Meal::getDateTime).reversed())
                 .collect(toList());
