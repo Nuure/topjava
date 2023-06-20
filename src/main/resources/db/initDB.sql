c228c427618dd99fbd152dcbcd8e549187f00ef6
alina.pirogova <alina.pirogova@team.wrike.com>
2023-06-20 22:11:38 +0300

    JdbcMealRepository implementation

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS user_role;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
+
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -21,5 +23,16 @@ CREATE TABLE user_role
     user_id INTEGER NOT NULL,
     role    VARCHAR NOT NULL,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id          INTEGER   NOT NULL PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER   NOT NULL,
+    date_time   TIMESTAMP NOT NULL,
+    description VARCHAR   NOT NULL,
+    calories    INTEGER   NOT NULL,
+    CONSTRAINT user_date_time_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,17 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2023-06-01 08:00:00', 'Breakfast of user', 350),
+       (100000, '2023-06-01 12:30:00', 'Lunch of user', 500),
+       (100000, '2023-06-01 18:00:00', 'Dinner of user', 700),
+       (100000, '2023-06-02 07:30:00', 'Breakfast of user', 1000),
+       (100000, '2023-06-02 13:00:00', 'Lunch of user', 1000),
+       (100000, '2023-06-02 19:30:00', 'Dinner of user', 800),
+       (100001, '2023-06-01 09:30:00', 'Breakfast of admin', 400),
+       (100001, '2023-06-01 13:00:00', 'Lunch of admin', 1000),
+       (100001, '2023-06-01 19:00:00', 'Dinner of admin', 1000),
+       (100001, '2023-06-02 08:00:00', 'Breakfast of admin', 350),
+       (100001, '2023-06-02 12:30:00', 'Lunch of admin', 500),
+       (100001, '2023-06-02 18:30:00', 'Dinner of admin', 700);
